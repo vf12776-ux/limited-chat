fdef90cdc99940a649d867ab803a551400e1e15e
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-05-05 00:26:30 +0300

    Fix PWA: minimal sw.js no cache, correct manifest

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,3 @@
-const CACHE_NAME = 'p2p-messenger-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-45.png',
-  '/icon-54.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
